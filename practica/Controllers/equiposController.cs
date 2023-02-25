ef62807cb3b01aa01044e3986893cbfdf66d3429
maurirod <mauricio.rodriguez4@catolica.edu.sv>
2023-02-25 12:18:19 -0600

    Agregar archivos de proyecto.

---
+++ practica/Controllers/equiposController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using practica.Models;
+using Microsoft.EntityFrameworkCore;
+namespace practica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+
+        public equiposController(equiposContext equipoContexto) {
+            _equiposContext= equipoContexto;
+        }
+        //Método de obtención de datos
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoequipo = (from e in _equiposContext.equipos select e).ToList();
+
+            if(listadoequipo.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoequipo);
+        }
+        //Método de búsqueda de ID
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContext.equipos
+                               where e.id == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null) return NotFound();
+            return Ok(equipo);
+        }
+
+        //Metodo de búsqueda
+        [HttpGet]
+        [Route("find/{filtro}")]
+        public IActionResult Get(string filtro) 
+        { 
+            List<equipos> equipo = (from e in _equiposContext.equipos
+                                    where e.nombre.Contains(filtro)
+                                    select e).ToList();
+            if (equipo == null) return NotFound();
+            return Ok(equipo);
+        }
+
+        //Método de añadir
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Post([FromBody] equipos equipo) {
+            try
+            {
+                _equiposContext.Add(equipo);
+                _equiposContext.SaveChanges();
+                return Ok(equipo);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //Método de actualizar datos
+        [HttpPut]
+        [Route("update/{id}")]
+        public IActionResult actualizarEquipo(int id, [FromBody] equipos updateDevice) {
+            
+            equipos? equipo = (from e in _equiposContext.equipos
+                               where e.id == id select e).FirstOrDefault();
+
+            if(equipo == null) return NotFound();
+
+            equipo.nombre = updateDevice.nombre;
+            equipo.costo = updateDevice.costo;
+
+            _equiposContext.Entry(equipo).State = EntityState.Modified; 
+            _equiposContext.SaveChanges();
+            return Ok(equipo);
+        }
+
+        //Métodos de eliminar por ID
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            equipos? equipo = (from e in _equiposContext.equipos
+                               where e.id == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null) return NotFound();
+
+            _equiposContext.equipos.Attach(equipo);
+            _equiposContext.equipos.Remove(equipo);
+            _equiposContext.SaveChanges();
+            return Ok(equipo);
+        }
+    }
+
+    
+
+}
---
+++ practica/Models/equipos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace practica.Models
+{
+    public class equipos
+    {
+        [Key]
+        public int id { set; get; }
+        public string nombre { set; get; }
+
+        public decimal costo { set; get; }
+    }
+}
